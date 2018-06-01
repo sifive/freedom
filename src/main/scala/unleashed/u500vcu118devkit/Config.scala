d548b5fa61729de5b6467931d80501409f4ed358
Wesley W. Terpstra <wesley@sifive.com>
2018-06-01 12:57:19 -0700

    rocket-chip: bump for API changes
    This required fixing up the build scripts and configs

--- src/main/scala/unleashed/u500vcu118devkit/Config.scala
+++ src/main/scala/unleashed/u500vcu118devkit/Config.scala
@@ -44,7 +44,7 @@ class U500VCU118DevKitConfig extends Config(
     case PeripheryBusKey => up(PeripheryBusKey, site).copy(frequency = 50000000) // 50 MHz hperiphery
     case MemoryXilinxDDRKey => XilinxVCU118MIGParams(address = Seq(AddressSet(0x80000000L,0x80000000L-1))) //2GB
     case DTSTimebase => BigInt(1000000)
-    case ExtMem => up(ExtMem).copy(size = 0x80000000L)
+    case ExtMem => up(ExtMem).map(_.copy(size = 0x80000000L))
     case JtagDTMKey => new JtagDTMConfig (
       idcodeVersion = 2,      // 1 was legacy (FE310-G000, Acai).
       idcodePartNum = 0x000,  // Decided to simplify.
